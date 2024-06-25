07cb4fe97a9d00a2014ee1d48f5f8a5b8347a7cf
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-06-25 10:02:33 -0300

    feat: added api request class

--- literalura/src/main/java/com/harcanjo/literalura/LiteraluraApplication.java
+++ literalura/src/main/java/com/harcanjo/literalura/LiteraluraApplication.java
@@ -1,13 +1,24 @@
 package com.harcanjo.literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.harcanjo.literalura.service.ConsumoApi;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {		
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://gutendex.com/books/1400/");
+		
+		System.out.println(json);
 	}
 
 }
